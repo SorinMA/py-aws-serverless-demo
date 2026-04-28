9b257a70732365822ca5f865d14fe35ba9d02c7e
unknown <sorinmartinescu9dev@gmail.com>
2026-04-28 19:41:19 +0300

    managed to login in frontend fuehhhh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,121 +2,61 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import { useAuth } from "react-oidc-context";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "7omb93dm674444e9s5mlkcrjai";
+    const logoutUri = "https://d2hrj96483he4t.cloudfront.net/";
+    const cognitoDomain = "https://backend-dev-544474104881-cupd.auth.eu-north-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <>
+        <section id="center">
+          <div className="hero">
+            <img src={heroImg} className="base" width="170" height="179" alt="" />
+            <img src={reactLogo} className="framework" alt="React logo" />
+            <img src={viteLogo} className="vite" alt="Vite logo" />
+          </div>
+          <div>
+            <h1>Aplicatia lu Sorinu</h1>
+          </div>
+        </section>
+        <section id="next-steps" style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+          <pre style={{ margin: 0 }}>Hello: {auth.user?.profile.email}</pre>
+          <pre style={{ margin: 0 }}>ID Token: {auth.user?.id_token}</pre>
+          <pre style={{ margin: 0 }}>Access Token: {auth.user?.access_token}</pre>
+          <pre style={{ margin: 0 }}>Refresh Token: {auth.user?.refresh_token}</pre>
+          <button onClick={() => auth.removeUser()}>Sign out</button>
+        </section>
+        <div className="ticks"></div>
+        <section id="spacer"></section>
+      </>
+    )
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started -- Aplicatia lu Sorinu</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <section id="center">
+      <div className="hero">
+        <button onClick={() => auth.signinRedirect()}>Sign in</button>
+        <button onClick={() => signOutRedirect()}>Sign out</button>
+      </div>
+    </section>
+  );
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from "react-oidc-context";
 import './index.css'
 import App from './App.jsx'
 
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_SEsuwO4XV",
+  client_id: "7omb93dm674444e9s5mlkcrjai",
+  redirect_uri: "https://d2hrj96483he4t.cloudfront.net/auth/callback",
+  response_type: "code",
+  scope: "email openid profile",
+};
+
+// https://d2hrj96483he4t.cloudfront.net/
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
