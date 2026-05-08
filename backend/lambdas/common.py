b0512293b48cfe97dbe5f692f976e09d0ee73485
MartinescuS <sorin.martinescu@vodafone.com>
2026-05-08 18:12:34 +0300

    - strict cors header

--- backend/lambdas/common.py
+++ backend/lambdas/common.py
@@ -6,14 +6,17 @@ DEFAULT_HEADERS = {"Content-Type": "application/json"}
 _EMAIL_PATTERN = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
 
 def json_response(status_code, body):
-    origin = os.environ.get("CORS_ALLOW_ORIGIN", "*")
+    origin = os.environ.get("CORS_ALLOW_ORIGIN")
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Authorization,Content-Type,X-Api-Key",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
+    }
+    if origin:
+        headers["Access-Control-Allow-Origin"] = origin
     return {
         "statusCode": status_code,
-        "headers": {
-            "Access-Control-Allow-Origin": origin,
-            "Access-Control-Allow-Headers": "Authorization,Content-Type,X-Api-Key",
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-        },
+        "headers": headers,
         "body": json.dumps(body),
     }
 
