10689a6be8968bce067365e5dace132520cfa8bd
MartinescuS <sorin.martinescu@vodafone.com>
2026-05-08 18:54:52 +0300

    - fix typo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ const cognitoAuthConfig = {
     authority: import.meta.env.VITE_OIDC_AUTHORITY,
     client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
     redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI,
-    post_logout_redirect_uri: import.meta.env.VITE_OIDC_POST_LOGOUT_REDIRECT_URL,
+    post_logout_redirect_uri: import.meta.env.VITE_OIDC_POST_LOGOUT_REDIRECT_URI,
     response_type: "code",
     scope: "email openid profile",
     userStore: new WebStorageStateStore({store: window.localStorage})
